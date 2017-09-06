68094a7c27ceb27b03b4def32d2831de5c494be8
Victoria-Winner <cool0193@mail.ru>
2017-09-06 21:58:50 +0300

    I don't want write on the language, but need(

---
+++ parcer.php
@@ -0,0 +1,11 @@
+<?php
+	function Parse($p1, $p2, $p3) {
+	 $num1 = strpos($p1, $p2);
+if (strpos($p1, $p2)=== false) return 0;		 
+	$num2 = substr($p1, $num);
+	return strip_tags(substr($num2,0, strpos($num2, $p3)));
+	
+	$String = file_get_contents('https://www.harvard.edu/');
+	echo Parse($String,'</div>');
+	
+?>
